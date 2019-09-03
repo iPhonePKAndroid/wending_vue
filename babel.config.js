e6baf4d2e12ae7501f62b2f2c7bd4f151e6202b2
问鼎 <postmaster@g000.cn>
2019-09-03 22:37:57 +0800

    a

--- babel.config.js
+++ babel.config.js
@@ -3,13 +3,5 @@ module.exports = {
     '@vue/app'
   ],
   plugins: [
-<<<<<<< HEAD
-=======
-    ['import', {
-      libraryName: 'vant',
-      libraryDirectory: 'es',
-      style: true
-    }, 'vant']
->>>>>>> 425629bc78a3a96b74b399d8f1a6a254d956c802
   ]
 }
