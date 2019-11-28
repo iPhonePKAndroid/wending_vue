5e194987fb854133ba6e6c95cb659e5be1e245eb
最帅的人 <postmaster@g000.cn>
2019-11-28 10:58:13 +0800

    没有数据的友好提示

--- src/views/Notice.vue
+++ src/views/Notice.vue
@@ -2,24 +2,26 @@
   <div class="about">
     <van-nav-bar title="平台公告" left-text="返回" left-arrow @click-left="onClickLeft" />
 
-    <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-      <div class="notice-list">
-        <div
-          class="notice-card"
-          v-for="item in noticeList"
-          :key="item.id"
-          @click="toDetail(item.id)"
-        >
-          <div class="time-title">
-            <span></span>
-            <span>{{item.created_at}}</span>
+    <van-list v-model="loading" :finished="finished" finished-text="加载完成" @load="onLoad">
+      <div v-if="noticeList.length > 0">
+        <div class="notice-list">
+          <div class="notice-card" v-for="item in noticeList" :key="item.id" @click="toDetail(item.id)" >
+            <div class="time-title">
+              <span></span>
+              <span>{{item.created_at}}</span>
+            </div>
+            <div>
+              <span class="title">{{item.name}}</span>
+            </div>
+            <div>
+              <span class="detail" v-html="item.desc"></span>
+            </div>
           </div>
-          <div>
-            <span class="title">{{item.name}}</span>
-          </div>
-          <div>
-            <span class="detail" v-html="item.desc"></span>
-          </div>
+        </div>
+      </div>
+      <div v-else>
+        <div class="no-notice">
+          暂无公告
         </div>
       </div>
     </van-list>
@@ -27,39 +29,39 @@
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      activeNames: ["1"],
-      loading: false,
-      finished: false,
-      page: 0,
-      noticeList: []
-    };
-  },
-  methods: {
-    onClickLeft() {
-      this.$router.go(-1);
+  export default {
+    data() {
+      return {
+        activeNames: ["1"],
+        loading: false,
+        finished: false,
+        page: 0,
+        noticeList: []
+      };
     },
-    onLoad() {
-      this.page++;
-      this.$axios.get(`notice?&page=${this.page}`).then(response => {
-        let ss = response.data;
-        this.noticeList = this.noticeList.concat(ss);
-        this.loading = false;
-        if (this.page >= response.last_page || response.total == 0) {
-          this.finished = true;
-        }
-      });
+    methods: {
+      onClickLeft() {
+        this.$router.go(-1);
+      },
+      onLoad() {
+        this.page++;
+        this.$axios.get(`notice?&page=${this.page}`).then(response => {
+          let ss = response.data;
+          this.noticeList = this.noticeList.concat(ss);
+          this.loading = false;
+          if (this.page >= response.last_page || response.total == 0) {
+            this.finished = true;
+          }
+        });
+      },
+      async toDetail(id) {
+        this.$router.push({
+          name: "noticeShow",
+          params: { id }
+        });
+      }
     },
-    async toDetail(id) {
-      this.$router.push({
-        name: "noticeShow",
-        params: { id }
-      });
-    }
-  },
-  mounted() {
+    mounted() {
     // this.getNoticeList();
   }
 };
@@ -67,6 +69,12 @@ export default {
 
 <style lang="scss">
 .about {
+  
+  .no-notice {
+    text-align: center;
+    margin-top: 1rem;
+  }
+
   font-size: 12px;
   .notice-list {
     margin: 10px;
@@ -112,6 +120,7 @@ export default {
       .detail {
         color: #666;
       }
+
     }
   }
 }
